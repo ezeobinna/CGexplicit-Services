f47afbf2491ee17c4e264bdf4e132932763c06de
Malone <107812723+ezeobinna@users.noreply.github.com>
2025-01-23 23:38:31 +0100

    added new

---
+++ script.js
@@ -0,0 +1,23 @@
+const primaryNav = document.querySelector(".navstylelink");
+const navToggle = document.querySelector(".mobilenavtoggle");
+const navstylelink = document.getElementById('navstylelink');
+const navbarbutton = document.getElementById('navbarbutton');
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
+
+// document.onclick = function(e){
+//     if(e.target.id !== 'navstylelink' && e.target.id !== 'navbarbutton' ) {
+//         primaryNav.setAttribute('data-visible', false);
+//         navToggle.setAttribute('aria-expanded', false);
+//     }
+// }
